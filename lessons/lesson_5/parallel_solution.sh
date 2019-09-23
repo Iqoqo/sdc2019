972337c4daf53e5000321edbedc0a6083af8ad2f
Raymond Lo <raymond.lo@samsungnext.com>
2019-09-23 14:26:04 +0300

    new video

--- lessons/lesson_5/parallel_solution.sh
+++ lessons/lesson_5/parallel_solution.sh
@@ -1,7 +1,7 @@
 set -x
 
 # split the toy video (3:06 minutes) into 30 seconds parts
-ffmpeg -i toy.mp4 -c copy -map 0 -segment_time 00:00:30 -f segment split%03d.mp4
+ffmpeg -i samsung10.mp4 -c copy -map 0 -segment_time 00:00:10 -f segment split%03d.mp4
 
 # run the glasses disco on the cloud and wait for compute to finish
 disco add --name parallel_video --script glasses_5.py --wait --run --download --input "split*mp4"
@@ -15,10 +15,10 @@ rm -f manifest.txt;
 for f in `ls *.mp4`; do echo "file '$f'" >> manifest.txt; done
 
 # stitch back the video
-ffmpeg -f concat -safe 0 -i manifest.txt -c copy discofy_toy.mp4
+ffmpeg -f concat -safe 0 -i manifest.txt -c copy discofy_samsung.mp4
 
 # move back the result to the lesson dir
-mv discofy_toy.mp4 ../..
+mv discofy_samsung.mp4 ../..
 
 # go back home
 cd ../..
