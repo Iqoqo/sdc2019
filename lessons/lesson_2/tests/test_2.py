a3b50b61e6b2c26d394d42a1dfb8cab6ab46cd54
Raymond Lo <raymond.lo@samsungnext.com>
2019-09-27 13:41:04 -0700

    test code for lesson2

--- lessons/lesson_2/tests/test_2.py
+++ lessons/lesson_2/tests/test_2.py
@@ -5,27 +5,27 @@ import filecmp
 
 
 def test_solution():
-    discofy_image_solution("michael.jpg",
-                           "disco-michael1.jpg")
+    discofy_image_solution("team.png",
+                           "disco-team.jpg")
 
-    assert os.path.exists("disco-michael1.jpg"), \
+    assert os.path.exists("disco-team.jpg"), \
         "oops, something's wrong with the solution"
 
 
 def test_compare():
-    discofy_image_solution("michael.jpg",
-                           "disco-michael1.jpg")
+    discofy_image_solution("team.png",
+                           "disco-team1.jpg")
 
-    assert os.path.exists("disco-michael1.jpg"), \
+    assert os.path.exists("disco-team1.jpg"), \
         "oops, something's wrong with the solution"
 
-    discofy_image("michael.jpg", "disco-michael2.jpg")
+    discofy_image("team.png", "disco-team2.jpg")
 
-    assert os.path.exists("disco-michael2.jpg"), \
+    assert os.path.exists("disco-team2.jpg"), \
         "Your code is not generating an output file"
 
-    assert filecmp.cmp("disco-michael1.jpg",
-                       "disco-michael2.jpg",
+    assert filecmp.cmp("disco-team1.jpg",
+                       "disco-team2.jpg",
                        shallow=False), "well, your solution did generate " \
                                        "something, but it is different from " \
                                        "our solution. open the images to " \
