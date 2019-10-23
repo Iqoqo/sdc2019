3abf111f5d0ef80271f021fb1632459a4b1e61ef
Raymond Lo <raymond.lo@samsungnext.com>
2019-10-23 14:06:16 -0700

    update docker

--- docker/update.sh
+++ docker/update.sh
@@ -1,6 +1,6 @@
 docker images
 docker ps -al
 docker commit <container id> iqoqo/discofy:sdc.local
-docker push
+docker push iqoqo/discofy:sdc.local
 docker tag iqoqo/discofy:sdc.local discofy:sdc.local
 
